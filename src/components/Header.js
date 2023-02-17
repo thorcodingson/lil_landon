5712d88cb2a9f10acd1fae774327cd19a05c77e8
Thor Codingson <markingmatt@gmail.com>
2023-02-17 12:29:36 +0100

    Added data in JSON format

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import menuLinksData from "./data/manu_links.json";
+
+const Header = () => {
+  return (
+    <header id="intro">
+      <article className="fullheight">
+        <div className="hgroup">
+          <h1>Landon Hotel</h1>
+          <h2>West London</h2>
+          <p>
+            <a href="#welcome">
+              <img
+                src="https://landonhotel.com/images/misc/arrow.png"
+                alt="down arrow"
+              />
+            </a>
+          </p>
+        </div>
+      </article>
+
+      <nav id="nav">
+        <div className="navbar">
+          <div className="brand">
+            <a href="#welcome">
+              Landon <span>Hotel</span>
+            </a>
+          </div>
+          <ul>
+            {menuLinksData.map((link) => (
+              <li key={link.class}>
+                <a className={`icon ${link.class}`} href={link.href}>
+                  <span>{link.text}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
